2af9b3f8c205163827b54cd07376fc4d446a19e8
javaswing <zxdlovejava@gmail.com>
2022-11-02 19:05:31 +0800

    feat: add ch02 validate

--- chap02/demo-spring/src/main/java/com/zxd/demo/controller/DesignTacoController.java
+++ chap02/demo-spring/src/main/java/com/zxd/demo/controller/DesignTacoController.java
@@ -2,12 +2,16 @@ package com.zxd.demo.controller;
 
 import com.zxd.demo.model.Ingredient;
 import com.zxd.demo.model.Ingredient.Type;
+import com.zxd.demo.model.Taco;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -24,18 +28,27 @@ public class DesignTacoController {
         List<Ingredient> ingredients = Arrays.asList(new Ingredient("FLTO", "面粉玉米饼", Type.WRAP), new Ingredient("COTO", "玉米饼", Type.WRAP), new Ingredient("GRBF", "碎牛肉", Type.PROTEIN));
 
         Type[] types = Type.values();
-        Map<String, List> allMap = new HashMap<String, List>();
+
 
         for (Type type : types) {
             String s = type.toString().toLowerCase();
-            allMap.put(s, filterByType(ingredients, type));
-
+            model.addAttribute(s, filterByType(ingredients, type));
         }
-        model.addAllAttributes(allMap);
-//        model.addAllAttributes("design", new Taco());
+
+        log.info(model.toString());
+        model.addAttribute("design", new Taco());
         return "design";
     }
 
+    @PostMapping
+    public String processDesign(@Valid Taco design, Errors errors) {
+        if(errors.hasErrors()) {
+            return "design";
+        }
+
+        log.info("processing: " + design);
+        return "redirect:/orders/current";
+    }
 
     private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
         return ingredients.stream().filter(x -> x.getType().equals(type)).collect(Collectors.toList());
