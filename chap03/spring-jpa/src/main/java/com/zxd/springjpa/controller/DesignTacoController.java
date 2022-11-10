89c78514493985c7b62fb55a4849a0c6d246f3c6
javaSwing <zxdlovejava@gmail.com>
2022-11-10 23:39:28 +0800

    feat: add spring-jpa

---
+++ chap03/spring-jpa/src/main/java/com/zxd/springjpa/controller/DesignTacoController.java
@@ -0,0 +1,86 @@
+package com.zxd.springjpa.controller;
+
+
+import com.zxd.springjpa.data.IngredientRepository;
+import com.zxd.springjpa.data.TacoRepository;
+import com.zxd.springjpa.model.Ingredient;
+import com.zxd.springjpa.model.Ingredient.Type;
+import com.zxd.springjpa.model.Order;
+import com.zxd.springjpa.model.Taco;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Controller
+@RequestMapping("/design")
+@SessionAttributes("order")
+public class DesignTacoController {
+
+    private final IngredientRepository ingredientRepo;
+
+    //end::ingredientRepoProperty[]
+    private TacoRepository designRepo;
+
+    @Autowired
+    public DesignTacoController(
+            IngredientRepository ingredientRepo,
+            TacoRepository designRepo) {
+        this.ingredientRepo = ingredientRepo;
+        this.designRepo = designRepo;
+    }
+
+    @ModelAttribute(name = "order")
+    public Order order() {
+        return new Order();
+    }
+
+    @ModelAttribute(name = "taco")
+    public Taco taco() {
+        return new Taco();
+    }
+
+
+    @GetMapping
+    public String showDesignForm(Model model) {
+        List<Ingredient> ingredients = new ArrayList<>();
+        ingredientRepo.findAll().forEach(i -> ingredients.add(i));
+
+        Type[] types = Ingredient.Type.values();
+        for (Type type : types) {
+            model.addAttribute(type.toString().toLowerCase(),
+                    filterByType(ingredients, type));
+        }
+
+        return "design";
+    }
+
+    @PostMapping
+    public String processDesign(
+            @Valid Taco design, Errors errors,
+            @ModelAttribute Order order) {
+
+        if (errors.hasErrors()) {
+            return "design";
+        }
+
+        Taco saved = designRepo.save(design);
+        order.addDesign(saved);
+
+        return "redirect:/orders/current";
+    }
+
+    private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
+        return ingredients.stream().filter(x -> x.getType().equals(type)).collect(Collectors.toList());
+    }
+}
