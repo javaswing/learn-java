390312f7998b5401aa6114325bf1f02f9943eb3c
javaswing <zxdlovejava@gmail.com>
2022-11-11 18:08:23 +0800

    feat: update SpringJdbcApplication

--- chap03/spring-jdbc/src/main/java/com/zxd/springjpa/SpringJdbcApplication.java
+++ chap03/spring-jdbc/src/main/java/com/zxd/springjpa/SpringJdbcApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringJpaApplication {
+public class SpringJdbcApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringJpaApplication.class, args);
+        SpringApplication.run(SpringJdbcApplication.class, args);
     }
 
 }
